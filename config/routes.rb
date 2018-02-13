67dfbf0c21acaf308f9052620e9cc43408fb9ee1
岩崎真士 <amehurisode@yahoo.co.jp>
2018-02-13 17:49:19 +0900

    group機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :user, only: [:edit, :update]
-  resources :group
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
